5523ff81ff1192ab7e7fb3e71b9f65487f629b7b
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-07 20:42:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону треугольника А:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону треугольника В:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону треугольника С:");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if ((a + b > c) && (b + c > a) && (c + a > b))
+                Console.WriteLine("Такой треугольник существует");
+            else Console.WriteLine("Такой треугольник не существует");
+        }
+    }
+}
